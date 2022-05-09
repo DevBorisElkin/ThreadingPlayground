1c9af06e80d563f425ed0620e7f9c384674deb0d
Boris <ea.foreverrr@gmail.com>
2022-05-09 23:50:49 +0300

    added some basic info

--- ThreadingPlayground.playground/Contents.swift
+++ ThreadingPlayground.playground/Contents.swift
@@ -1,5 +1,45 @@
 import UIKit
 
-var greeting = "Hello, playground"
+DispatchQueue.main.async{
+    // in app use
+    // self.foo()
+    
+    foo()
+}
 
-print(greeting)
+DispatchQueue.global(qos: .background).async{
+    // fetch data from api
+    
+    DispatchQueue.main.async{
+        //update UI
+    }
+}
+
+func foo(){
+    
+}
+
+/*
+ print("1")
+ DispatchQueue.global().async {
+    print("2")
+    DispatchQueue.main.async {
+         print("3")
+         DispatchQueue.global().sync {
+            print("4")
+            DispatchQueue.main.sync {
+                print("5")
+            }
+            print("6")
+        }
+        print("7")
+    }
+    print("8")
+ }
+ print("9")
+ 
+ Ответ: 1, 9, 2, 8, 3, 4, deadlock
+ Вопрос: Как убрать deadlock?
+ Ответ: убрать любой main, main на main = deadlock
+ 
+ */
