32f7cf5f774dcf1dc14ce97ee1f0da26d4d2210c
Boris <ea.foreverrr@gmail.com>
2022-05-10 11:04:53 +0300

    Added simple dispatch group example

--- ThreadingPlayground.playground/Contents.swift
+++ ThreadingPlayground.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+// DispatchQueues below
+
 DispatchQueue.main.async{
     // in app use
     // self.foo()
@@ -43,3 +45,40 @@ func foo(){
  Ответ: убрать любой main, main на main = deadlock
  
  */
+
+
+// DispatchGroups below
+
+func getData(){
+    let urls = [
+        "https://api.google.com/1",
+        "https://api.google.com/2",
+        "https://api.google.com/3"
+    ]
+    
+    let group = DispatchGroup()
+    
+    for url in urls{
+        guard let url = URL(string: url) else{
+            continue
+        }
+        
+        group.enter()
+        let task = URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
+            defer{
+                group.leave()
+            }
+            
+            guard let data = data else{
+                return
+            }
+            print(data)
+        })
+        
+        task.resume()
+    }
+    
+    group.notify(queue: .main, execute: {
+        // Update User Interface
+    })
+}
